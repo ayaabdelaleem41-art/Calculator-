b3210bf8059e5e7e28d5a55b36a5b135515af8ae
ayaabdelaleem41-art <ayaabdelaleem41@gmail.com>
2025-11-16 17:04:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-// نجيب شاشة العرض
-const display = document.getElementById("display");
 
-// نجيب كل الأزرار
+const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
-// نضيف لكل زر حدث الضغط عليه
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonText = button.textContent;
 
-    // لو المستخدم ضغط على "C" → نمسح كل حاجة
     if (buttonText === "C") {
       display.value = "";
     }
 
-    // لو المستخدم ضغط على "DEL" → نحذف آخر رقم أو رمز
+  
     else if (buttonText === "DEL") {
       display.value = display.value.slice(0, -1);
     }
 
-    // لو المستخدم ضغط على "=" → نطلع النتيجة
+    
     else if (buttonText === "=") {
       try {
         display.value = eval(display.value);
@@ -28,7 +24,6 @@ buttons.forEach((button) => {
       }
     }
 
-    // غير كده → نضيف الرقم أو العملية اللي ضغط عليها
     else {
       display.value += buttonText;
     }
